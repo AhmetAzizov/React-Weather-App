924f9ec00af6bf067e03a6d8d44b0d35447a41b8
AhmetAzizov <ahmet.azizov@gmail.com>
2023-11-28 01:07:55 +0100

    Updated error handling

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import { Spinner } from 'react-bootstrap';
 import OpenWeatherMap from 'openweathermap-ts';
 import { CurrentResponse } from 'openweathermap-ts/dist/types';
 import InfoComponent from './components/InfoComponent';
+import { BsSortDown } from "react-icons/bs";
+import { FaTemperatureArrowDown, FaTemperatureArrowUp, FaTemperatureQuarter } from "react-icons/fa6";
+import { GiWindsock } from "react-icons/gi";
+
 
 const openWeather = new OpenWeatherMap({
   apiKey: process.env.REACT_APP_OPENWEATHER_API_KEY!
@@ -16,22 +20,23 @@ function App() {
 
   const [weatherIcon, setWeatherIcon] = useState<string>("https://openweathermap.org/img/wn/04d@2x.png");
   const [loading, setLoading] = useState(true);
-  const [weatherData, setWeatherData] = useState<CurrentResponse | null>(null);
+  const [weatherData, setWeatherData] = useState<CurrentResponse | Error | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [weatherInput, setWeatherInput] = useState<string>("");
 
   useEffect(() => {
     (async () => {
       try {
         const city = await getCurrentLocation();
         const data = await checkWeather(city);
+        console.log(data);
         setWeatherData(data);
         setLoading(false);
-      } catch (error) {
-        console.error(error);
-        alert(error);
+      } catch (error: any) {
+        setError(error?.message);
       }
     })();
   }, [])
-
 
   function getCurrentLocation(): Promise<string> {
     return new Promise((resolve, reject) => {
@@ -47,11 +52,14 @@ function App() {
             try {
               const response = await fetch(api_url);
               const data = await response.json();
+
+              if ((data.results as []).length === 0) {
+                reject(new Error("Sorry, we couldn't find weather data of your current location"))
+              }
+
               const city = data.results[0].components.city;
               resolve(city);
             } catch (error) {
-              console.error(error);
-              alert(error);
               reject(error);
             }
           },
@@ -66,50 +74,122 @@ function App() {
   }
 
 
-  async function checkWeather(city: string): Promise<CurrentResponse> {
+  async function checkWeather(city: string): Promise<CurrentResponse | Error> {
     try {
       const weather = await openWeather.getCurrentWeatherByCityName({
         cityName: city
       });
 
-      setWeatherIcon("https://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png")
+      setWeatherIcon("https://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png");
+      console.log(weather);
       return weather;
     } catch (error) {
-      throw Error("" + error);
+      if (error instanceof Error) {
+        return error;
+      } else {
+        throw Error("some error");
+      }
     }
   }
 
 
   return (
     <>
-      <Spinner className='loadingSpinner' hidden={!loading}></Spinner>
+      {error &&
+        <p>Error: {error}</p>
+      }
 
-      {!loading && weatherData &&
+      {!error &&
         <>
-          <div className="inputSection">
-            <input className='weatherInput' placeholder='Search for a City' />
-            <button className='inputButton'><IoSearch /></button>
-          </div>
+          <Spinner className='loadingSpinner' animation='border' hidden={loading} variant='primary'></Spinner>
 
-          <div className="card">
+          {!loading && weatherData && !(weatherData instanceof Error) &&
+            <>
+              <div className="inputSection">
+                <input className='weatherInput' placeholder='Search for a City' value={weatherInput} onChange={(e) => setWeatherInput(e.target.value)} />
+                <button className='inputButton' onClick={async () => {
+                  try {
+                    const data = await checkWeather(weatherInput);
+                    setWeatherData(data);
+                    // setLoading(false);
+                  } catch (error) {
+                    if (error instanceof Error) setError(error.message);
+                    console.error(error);
+                  }
+                }}><IoSearch /></button>
+              </div>
 
-            <img className='weatherIcon' src={weatherIcon} alt='weather icon' />
+              <div className="mainWrapper">
 
-            <p className='cityName'>{weatherData.name}</p>
-            <p className='temperature'>{weatherData.main.temp + " °C"}</p>
+                <div className="contentWrapper card primaryWrapper">
 
+                  <p className='cityName'>{weatherData.name}</p>
 
-            <div className="infoSection">
-              <InfoComponent
-                data={weatherData.main.humidity.toString()}
-                Icon={MdOutlineWaves}
-              />
-              <InfoComponent
-                data={weatherData.wind.speed.toString()}
-                Icon={FaWind}
-              />
-            </div>
-          </div>
+                  <InfoComponent
+                    className='main-info'
+                    data={weatherData.main.temp.toString() + "°C"}
+                    label="temperature"
+                    Icon={FaTemperatureQuarter}
+                  />
+
+                  <InfoComponent
+                    className='main-info'
+                    data={weatherData.main.feels_like.toString() + "°C"}
+                    label="feels like"
+                    Icon={FaTemperatureQuarter}
+                  />
+
+                  <div className="infoWrapper">
+                    <InfoComponent
+                      data={weatherData.wind.deg.toString() + "°"}
+                      label="wind degree"
+                      Icon={GiWindsock}
+                    />
+                    <InfoComponent
+                      data={weatherData.wind.speed.toString() + "km/h"}
+                      label="wind speed"
+                      Icon={FaWind}
+                    />
+                  </div>
+                </div>
+
+                <div className="contentWrapper secondaryWrapper">
+
+                  <div className="weatherType card subCard">
+                    <img className='weatherIcon' src={weatherIcon} alt='weather icon' />
+                    <p className="weatherDescription">{weatherData.weather[0].description}</p>
+                  </div>
+
+                  <div className="subCard card weatherDetails">
+                    <div className="infoWrapper">
+                      <InfoComponent
+                        data={weatherData.main.temp_min.toString() + " °C"}
+                        label="min temperature"
+                        Icon={FaTemperatureArrowDown}
+                      />
+                      <InfoComponent
+                        data={weatherData.main.temp_max.toString() + " °C"}
+                        label="max temperature"
+                        Icon={FaTemperatureArrowUp}
+                      />
+                    </div>
+                    <div className="infoWrapper">
+                      <InfoComponent
+                        data={weatherData.main.humidity.toString() + "%"}
+                        label="humidity"
+                        Icon={MdOutlineWaves}
+                      />
+                      <InfoComponent
+                        data={weatherData.main.pressure.toString() + " Pa"}
+                        label="pressure"
+                        Icon={BsSortDown}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          }
         </>
       }
     </>
--- src/components/InfoComponent.tsx
+++ src/components/InfoComponent.tsx
@@ -1,18 +1,21 @@
 import { IconType } from "react-icons/lib";
+import styles from '../styles/InfoComponent.module.css'
 
 interface infoComponentProps {
+    className?: string,
     data: string,
+    label: string,
     Icon: IconType
 }
 
-const InfoComponent = ({ data, Icon }: infoComponentProps) => {
+const InfoComponent = ({ className, data, label, Icon }: infoComponentProps) => {
     return (
         <>
-            <div className="info">
-                <p className='infoLabel'>temperature</p>
-                <section className="valueIconWrapper">
+            <div className={`${styles.info} ${className}`}>
+                <p className={styles.infoLabel}>{label}</p>
+                <section className={styles.valueIconWrapper}>
                     <Icon className='infoIcon' />
-                    <p className="infoValue">{data + " %"}</p>
+                    <p className="infoValue">{data}</p>
                 </section>
             </div>
         </>
